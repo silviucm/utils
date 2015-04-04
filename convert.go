b61f0af2435e69ed549ad8610937589faddde743
silviucm <silviucapota@hotmail.com>
2015-04-04 00:41:56 -0400

    Int32ToString and StringToInt32

--- convert.go
+++ convert.go
@@ -20,3 +20,18 @@ func (dummyReceiver *convertUtils) StringToInt64(valueAsString string) (int64, e
 	return strconv.ParseInt(valueAsString, 10, 64)
 
 }
+
+// Converts int32 to string
+func (dummyReceiver *convertUtils) Int32ToString(integerValue int32) string {
+
+	return strconv.FormatInt(int64(integerValue), 10)
+
+}
+
+// Converts a string to int32
+func (dummyReceiver *convertUtils) StringToInt32(valueAsString string) (int32, error) {
+
+	i64, err := strconv.ParseInt(valueAsString, 10, 32)
+	return int32(i64), err
+
+}
